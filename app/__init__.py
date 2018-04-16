52a81c2f431d46387eaa28eb1bd984655fbca4e0
Andrew Nguyen <ashewx@gmail.com>
2018-04-15 19:04:50 -0700

    First Commit.

---
+++ app/__init__.py
@@ -0,0 +1,31 @@
+from __future__ import print_function
+#from future.standard_library import install_aliases
+#install_aliases()
+
+from urllib.parse import urlparse, urlencode
+from urllib.request import urlopen, Request
+from urllib.error import HTTPError
+
+import json
+import os
+
+from flask import Flask, request, make_response, render_template
+from pip._vendor.requests.sessions import session
+
+# Flask app should start in global layout
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    return null
+
+if __name__ == '__main__':
+    port = int(os.getenv('PORT', 5000))
+
+#     print("Starting app on port %d" % port)
+
+app.run(debug=True, port=port, host='0.0.0.0')
---
+++ run.py
@@ -0,0 +1,8 @@
+import os
+from app import app
+
+
+if __name__ == '__main__':
+    port = int(os.getenv('PORT', 5000))
+    app.run(debug=True, port=port, host='0.0.0.0')
+#     print("Starting app on port %d" % port)
