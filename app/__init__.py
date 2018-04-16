d30640e739aaefa58e15565e1ac6458e7c86bd35
Andrew Nguyen <ashewx@gmail.com>
2018-04-15 19:07:07 -0700

    Another commit

--- app/__init__.py
+++ app/__init__.py
@@ -23,9 +23,3 @@ def index():
 def webhook():
     return null
 
-if __name__ == '__main__':
-    port = int(os.getenv('PORT', 5000))
-
-#     print("Starting app on port %d" % port)
-
-app.run(debug=True, port=port, host='0.0.0.0')
