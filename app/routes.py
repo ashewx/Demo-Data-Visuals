6b8ace9336c017c5808b6d4ea4c7a376cf0e185c
Matt Davis <mwdavis3@asu.edu>
2018-04-19 17:18:13 -0700

    Added count method

--- app/routes.py
+++ app/routes.py
@@ -7,23 +7,22 @@ import decimal, datetime
 @app.route('/')
 @app.route('/index')
 def index():
-	return render_template('index.html')
+	return render_template('index.html')        
         
-        
-@app.route('/webhook/<query>', methods=['GET'])
-def webhook(query):
-	#res = {
-	#	"message": query,
-	#	"check": 123
-	#}
-	#res = json.dumps(res, indent=4)
-
-	#r = make_response(res)
-	#r.headers['Content-Type'] = 'application/json'
-	
+@app.route('/average', methods=['GET'])
+def average(query):	
 	dbengine = create_engine("postgres://gkkjrzsf:wN_AqVb_CsfndXNEn_j-l-cwZz86VPtU@tantor.db.elephantsql.com:5432/gkkjrzsf")
 	result = dbengine.execute("SELECT g.name, AVG(r.rating) AS rating FROM genre g, movies m, ratings r, hasagenre h WHERE g.genreid=h.genreid AND m.movieid=h.movieid AND m.movieid=r.movieid GROUP BY g.name ORDER BY g.name;")
 	#print (result)
+	r = (json.dumps([dict(r) for r in result], default=alchemyencoder))
+	r = make_response(r)
+	r.headers['Content-Type'] = 'application/json'
+	return r
+	
+@app.route('/count', methods=['GET'])
+def count():	
+	dbengine = create_engine("postgres://gkkjrzsf:wN_AqVb_CsfndXNEn_j-l-cwZz86VPtU@tantor.db.elephantsql.com:5432/gkkjrzsf")
+	result = dbengine.execute("SELECT g.name, COUNT(*) AS moviecount FROM genre g, movies m, hasagenre h WHERE g.genreid=h.genreid AND m.movieid=h.movieid GROUP BY g.name ORDER BY g.name;")
 	r = (json.dumps([dict(r) for r in result], default=alchemyencoder))
 	r = make_response(r)
 	r.headers['Content-Type'] = 'application/json'
