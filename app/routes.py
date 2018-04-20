7059496aef0a81272521f11dd7a9b94e9212c30d
Matt Davis <mwdavis3@asu.edu>
2018-04-19 18:15:34 -0700

    Added initial average functionality

--- app/routes.py
+++ app/routes.py
@@ -13,9 +13,19 @@ def index():
 	return render_template('index.html')        
         
 @app.route('/average', methods=['GET'])
-def average(query):	
-	dbengine = create_engine("postgres://gkkjrzsf:wN_AqVb_CsfndXNEn_j-l-cwZz86VPtU@tantor.db.elephantsql.com:5432/gkkjrzsf")
-	result = dbengine.execute("SELECT g.name, AVG(r.rating) AS rating FROM genre g, movies m, ratings r, hasagenre h WHERE g.genreid=h.genreid AND m.movieid=h.movieid AND m.movieid=r.movieid GROUP BY g.name ORDER BY g.name;")
+def average():	
+	# Retrieve URL parameters
+	min = request.args.get('min', None)  # use default value replace 'None'
+	max = request.args.get('max', None)
+	title_keyword = request.args.get('title', None)
+	tag_keyword = request.args.get('tag', None)
+	
+	# Generate the SQL query for given parameters
+	if min == None and max == None and title_keyword == None and tag_keyword == None:
+		sql_query = "SELECT g.name, AVG(r.rating) AS rating FROM genre g, movies m, ratings r, hasagenre h WHERE g.genreid=h.genreid AND m.movieid=h.movieid AND m.movieid=r.movieid GROUP BY g.name ORDER BY g.name;"
+	else:
+		sql_query = ""
+	result = dbengine.execute(sql_query)
 	#print (result)
 	r = (json.dumps([dict(r) for r in result], default=alchemyencoder))
 	r = make_response(r)
@@ -33,8 +43,10 @@ def count():
 	# Generate the SQL query for given parameters
 	if min == None and max == None and title_keyword == None and tag_keyword == None:
 		sql_query = "SELECT g.name, COUNT(*) AS moviecount FROM genre g, movies m, hasagenre h WHERE g.genreid=h.genreid AND m.movieid=h.movieid GROUP BY g.name ORDER BY g.name;"
+	elif min != None and max != None:
+		sql_query = ""
 	else:
-		return "Error"
+		sql_query = ""
 	result = dbengine.execute(sql_query)
 	r = (json.dumps([dict(r) for r in result], default=alchemyencoder))
 	r = make_response(r)
