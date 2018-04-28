a7e96c68aa000a0546bfa787589169841cd914b2
Matt Davis <mwdavis3@asu.edu>
2018-04-28 12:12:49 -0700

    Removed all null-keyword from routes.py

--- app/routes.py
+++ app/routes.py
@@ -70,15 +70,10 @@ def count():
 	max = request.args.get('max', None)
 	title_keyword = request.args.get('title', None)
 	tag_keyword = request.args.get('tag', None)
-	null_keyword = request.args.get('null', None)
-	if null_keyword == "true":
-		null_keyword = True
-	else:
-		null_keyword = False
 	
 	# Generate the SQL query for given parameters
 	if min == None and max == None and title_keyword == None and tag_keyword == None: # /count
-		sql_query = "SELECT g.name, COUNT(*) AS moviecount" \
+		sql_query = "SELECT g.name, COUNT(*) AS ratingcount" \
 					" FROM genre g, movies m, hasagenre h, ratings r" \
 					" WHERE g.genreid=h.genreid AND m.movieid=h.movieid AND m.movieid=r.movieid" \
 					" GROUP BY g.name" \
@@ -86,58 +81,30 @@ def count():
 	else:
 		if min != None and max != None:
 			if title_keyword != None: 
-				if tag_keyword != None: 
-					if null_keyword: # /count?min=<min>&max=<max>&title=<title_keyword>&tag=<tag_keyword>&null=<null_keyword>
-						sql_query = "SELECT g.name, COUNT(m.movieid) AS moviecount" \
-									" FROM genre g, movies m, hasagenre h, tags t, taginfo ti" \
-									" WHERE g.genreid=h.genreid AND m.movieid=h.movieid AND m.movieid=t.movieid AND t.tagid=ti.tagid AND LOWER(m.title) LIKE LOWER('%%{2}%%') AND LOWER(ti.content) LIKE LOWER('%%{3}%%') AND (m.movieid IN (SELECT r.movieid FROM ratings r WHERE r.rating >= {0} AND r.rating <= {1}) OR m.movieid NOT IN (SELECT r1.movieid FROM ratings r1))" \
-									" GROUP BY g.name" \
-									" ORDER BY g.name;".format(min, max, title_keyword, tag_keyword)
-					else: # /count?min=<min>&max=<max>&title=<title_keyword>&tag=<tag_keyword>
-						sql_query = "SELECT g.name, COUNT(m.movieid) AS moviecount" \
-									" FROM genre g, movies m, ratings r, hasagenre h, tags t, taginfo ti" \
-									" WHERE g.genreid=h.genreid AND m.movieid=h.movieid AND m.movieid=r.movieid AND m.movieid=t.movieid AND t.tagid=ti.tagid AND r.rating >= {0} AND r.rating <= {1} AND LOWER(m.title) LIKE LOWER('%%{2}%%') AND LOWER(ti.content) LIKE LOWER('%%{3}%%')" \
-									" GROUP BY g.name" \
-									" ORDER BY g.name;".format(min, max, title_keyword, tag_keyword)
-				else: 
-					if null_keyword: # /count?min=<min>&max=<max>&title=<title_keyword>&null=<null_keyword>
-						sql_query = "SELECT g.name, COUNT(m.movieid) AS moviecount" \
-									" FROM genre g, movies m, hasagenre h" \
-									" WHERE g.genreid=h.genreid AND m.movieid=h.movieid AND LOWER(m.title) LIKE LOWER('%%{2}%%') AND (m.movieid IN (SELECT r.movieid FROM ratings r WHERE r.rating >= {0} AND r.rating <= {1}) OR m.movieid NOT IN (SELECT r1.movieid FROM ratings r1))" \
-									" GROUP BY g.name" \
-									" ORDER BY g.name;".format(min, max, title_keyword)
-					else: # /count?min=<min>&max=<max>&title=<title_keyword>
-						sql_query = "SELECT g.name, COUNT(m.movieid) AS moviecount" \
-									" FROM genre g, movies m, ratings r, hasagenre h" \
-									" WHERE g.genreid=h.genreid AND m.movieid=h.movieid AND m.movieid=r.movieid AND r.rating >= {0} AND r.rating <= {1} AND LOWER(m.title) LIKE LOWER('%%{2}%%')" \
-									" GROUP BY g.name" \
-									" ORDER BY g.name;".format(min, max, title_keyword)
-			elif tag_keyword != None:
-				if null_keyword: # /count?min=<min>&max=<max>&tag=<tag_keyword>&null=<null_keyword>
-					sql_query = "SELECT g.name, COUNT(m.movieid) AS moviecount" \
-								" FROM genre g, movies m, hasagenre h, tags t, taginfo ti" \
-								" WHERE g.genreid=h.genreid AND m.movieid=h.movieid AND m.movieid=t.movieid AND t.tagid=ti.tagid AND LOWER(ti.content) LIKE LOWER('%%{2}%%') AND (m.movieid IN (SELECT r.movieid FROM ratings r WHERE r.rating >= {0} AND r.rating <= {1}) OR m.movieid NOT IN (SELECT r1.movieid FROM ratings r1))" \
-								" GROUP BY g.name" \
-								" ORDER BY g.name;".format(min, max, tag_keyword)
-				else:  # /count?min=<min>&max=<max>&tag=<tag_keyword>
-					sql_query = "SELECT g.name, COUNT(m.movieid) AS moviecount" \
+				if tag_keyword != None: # /count?min=<min>&max=<max>&title=<title_keyword>&tag=<tag_keyword>
+					sql_query = "SELECT g.name, COUNT(m.movieid) AS ratingcount" \
 								" FROM genre g, movies m, ratings r, hasagenre h, tags t, taginfo ti" \
-								" WHERE g.genreid=h.genreid AND m.movieid=h.movieid AND m.movieid=r.movieid AND m.movieid=t.movieid AND t.tagid=ti.tagid AND r.rating >= {0} AND r.rating <= {1} AND LOWER(ti.content) LIKE LOWER('%%{2}%%')" \
+								" WHERE g.genreid=h.genreid AND m.movieid=h.movieid AND m.movieid=r.movieid AND m.movieid=t.movieid AND t.tagid=ti.tagid AND r.rating >= {0} AND r.rating <= {1} AND LOWER(m.title) LIKE LOWER('%%{2}%%') AND LOWER(ti.content) LIKE LOWER('%%{3}%%')" \
 								" GROUP BY g.name" \
-								" ORDER BY g.name;".format(min, max, tag_keyword)
-			else: 
-				if null_keyword: # /count?min=<min>&max=<max>&null=<null_keyword>
-					sql_query = "SELECT g.name, COUNT(m.movieid) AS moviecount" \
-								" FROM genre g, movies m, hasagenre h" \
-								" WHERE g.genreid=h.genreid AND m.movieid=h.movieid AND (m.movieid IN (SELECT r.movieid FROM ratings r WHERE r.rating >= {0} AND r.rating <= {1}) OR m.movieid NOT IN (SELECT r1.movieid FROM ratings r1))" \
-								" GROUP BY g.name" \
-								" ORDER BY g.name;".format(min, max)
-				else: # /count?min=<min>&max=<max>
-					sql_query = "SELECT g.name, COUNT(m.movieid) AS moviecount" \
+								" ORDER BY g.name;".format(min, max, title_keyword, tag_keyword)
+				else: # /count?min=<min>&max=<max>&title=<title_keyword>
+					sql_query = "SELECT g.name, COUNT(m.movieid) AS ratingcount" \
 								" FROM genre g, movies m, ratings r, hasagenre h" \
-								" WHERE g.genreid=h.genreid AND m.movieid=h.movieid AND m.movieid=r.movieid AND r.rating >= {0} AND r.rating <= {1}" \
+								" WHERE g.genreid=h.genreid AND m.movieid=h.movieid AND m.movieid=r.movieid AND r.rating >= {0} AND r.rating <= {1} AND LOWER(m.title) LIKE LOWER('%%{2}%%')" \
 								" GROUP BY g.name" \
-								" ORDER BY g.name;".format(min, max)
+								" ORDER BY g.name;".format(min, max, title_keyword)
+			elif tag_keyword != None: # /count?min=<min>&max=<max>&tag=<tag_keyword>
+				sql_query = "SELECT g.name, COUNT(m.movieid) AS ratingcount" \
+							" FROM genre g, movies m, ratings r, hasagenre h, tags t, taginfo ti" \
+							" WHERE g.genreid=h.genreid AND m.movieid=h.movieid AND m.movieid=r.movieid AND m.movieid=t.movieid AND t.tagid=ti.tagid AND r.rating >= {0} AND r.rating <= {1} AND LOWER(ti.content) LIKE LOWER('%%{2}%%')" \
+							" GROUP BY g.name" \
+							" ORDER BY g.name;".format(min, max, tag_keyword)
+			else: # /count?min=<min>&max=<max>
+				sql_query = "SELECT g.name, COUNT(m.movieid) AS ratingcount" \
+							" FROM genre g, movies m, ratings r, hasagenre h" \
+							" WHERE g.genreid=h.genreid AND m.movieid=h.movieid AND m.movieid=r.movieid AND r.rating >= {0} AND r.rating <= {1}" \
+							" GROUP BY g.name" \
+							" ORDER BY g.name;".format(min, max)
 
 	# Retrieve Json string and format to Json object
 	result = dbengine.execute(sql_query)
